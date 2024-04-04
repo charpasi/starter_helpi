42f80348e40324996454171e2d9671395a00cdea
charpasi <charpasi@udel.edu>
2024-04-04 12:19:57 -0400

    added detailed questions page

---
+++ src/pages/DetailedQuestions.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function DetailedQuestions() {
+    return (
+        <div>
+            <Header/>
+            <p>Detailed Questions</p>
+            <Footer/>
+        </div>
+    );
+}
+
+export default DetailedQuestions;
