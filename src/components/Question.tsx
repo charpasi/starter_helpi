5a246e76299ae62e5b5642d099e42047255142a6
charpasi <charpasi@udel.edu>
2024-04-10 17:12:14 -0400

    question arrays file

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,11 +1,11 @@
 import { Button } from "react-bootstrap";
 import "./Question.css";
 import React, { useState } from 'react';
+import { detailedQuestions } from './QuestionArrays';
 
 
 export function Question(): JSX.Element {
-    const [questions, setQuestions] = useState<string[]>(["What moral values are most important to you in your career?", 
-    "What extracurricular activities or hobbies do you enjoy in your free time?", "What subjects or topics do you excel at in school or work?"]);
+    const [questions, setQuestions] = useState<string[]>(detailedQuestions);
     const [index, setIndex] = useState(0);
 
     const nextClick = () => {
@@ -25,15 +25,15 @@ export function Question(): JSX.Element {
         }
     }
     return (
-        <header className="Question"> 
-            <h2>Question:</h2>
+        <div className="Question"> 
+            <h2>Question {index + 1}:</h2>
             <div style={ {border: '1px solid purple', padding: '4px'} }>
                 <p>{ questions[index] }</p>
             </div>
             <Button onClick={previousClick} disabled={index === 0}>Previous</Button>
             <Button onClick={nextClick} disabled={index === questions.length - 1}>Next</Button>
             <Button onClick={nextClick} disabled={index !== questions.length - 1}>Finish</Button>
-        </header>
+        </div>
     );
 }
 
