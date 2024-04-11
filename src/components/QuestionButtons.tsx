6f32d0800fabdd62b8725bf9a108627036299989
charpasi <charpasi@udel.edu>
2024-04-11 15:41:19 -0400

    buttons moved to own component

---
+++ src/components/QuestionButtons.tsx
@@ -0,0 +1,24 @@
+import { Button } from "react-bootstrap";
+import "./Question.css";
+import React, { useState } from 'react';
+
+interface ButtonProps {
+    onNext: () => void;
+    onPrevious: () => void;
+    current: number;
+    length: number;
+}
+
+export const QuestionButtons: React.FC<ButtonProps> = ({onNext, onPrevious, current, length}) => {
+    return (
+        <div className="QuestionButtons"> 
+            <div className="button-container">
+            <Button onClick={onPrevious} disabled={current === 0}>Previous</Button>
+            <Button onClick={onNext} disabled={current === length - 1}>Next</Button>
+            <Button onClick={onNext} disabled={current !== length - 1}>Finish</Button>
+            </div>
+        </div>
+    );
+}
+
+export default QuestionButtons;
--- src/pages/DetailedQuestions.tsx
+++ src/pages/DetailedQuestions.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import { Page } from "../App";
-import Question from "../components/Question";
 import { detailedQuestions } from "../components/Question"
+import Question from "../components/Question";
 import TextInput from "../components/TextInput";
+import QuestionButtons from "../components/QuestionButtons";
 
 
 function DetailedQuestions( {
     setCurrentPage
 }: {
     setCurrentPage: (pageName: Page) => void
-}) {return (
+}) {
+    const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+    const handleNext = () => {
+        setCurrentQuestion(prevIndex => prevIndex + 1);
+    };
+    const handlePrevious = () => {
+        setCurrentQuestion(prevIndex => prevIndex - 1);
+      };
+    return (
         <div className="DetailedPage">
              <h1>Detailed Questions </h1>
-             <Question questionArray = {detailedQuestions}/>
+             <Question current = {currentQuestion} questionArray = { detailedQuestions}/>
              <TextInput/>
+             <QuestionButtons onNext = {handleNext} onPrevious = {handlePrevious} current={currentQuestion} length = {detailedQuestions.length} />
         </div>
     );
 }
