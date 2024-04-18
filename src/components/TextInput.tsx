067582d3678c047c0542070f568e85e7e57c2b1b
Matthew Nadar <BOGUS@EMAIL>
2024-04-18 00:16:05 -0400

    Changed textinput to have a placeholder so that when you click on it you don't need to manually delete the text in the box

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -7,17 +7,20 @@ interface TextInputProps {
 }
 
 export function TextInput({ currentQuestion }: TextInputProps): JSX.Element  {
-    const [text, setText] = useState<string>('Enter response here');
+    const [text, setText] = useState<string>(''); 
+
     // Reset text when the current question changes
     useEffect(() => {
-        setText('Enter response here');
+        setText('');
     }, [currentQuestion]); 
+
     return (
         <div className="TextInput"> 
         <Form.Group controlId="formTextInput" as={Row} >
         <Form.Label column sm={2}></Form.Label>
         <Col>
         <Form.Control
+        placeholder="Enter your response here."
         value={text}
         onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
         setText(event.target.value)} 
