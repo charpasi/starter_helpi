2d8bebfe558e499c330f8f46fed05cb8508cb49a
Matthew Nadar <BOGUS@EMAIL>
2024-04-18 11:43:19 -0400

    Fixed answers saving between next/previous for basic quiz?

--- src/components/MultipleChoiceInput.tsx
+++ src/components/MultipleChoiceInput.tsx
@@ -1,13 +1,13 @@
-import {Form} from "react-bootstrap";
-import React, {useEffect, useState } from 'react';
+import { Form } from "react-bootstrap";
+import React from 'react';
 import "./MultipleChoiceInput.css";
 
 interface MultipleChoiceInputProps {
-    currentQuestion: number; 
+    selectedOption: string | null;
+    handleOptionChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function MultipleChoiceInput({ currentQuestion }: MultipleChoiceInputProps): JSX.Element {
-    const [selectedOption, setSelectedOption] = useState<string>("");
+export function MultipleChoiceInput({ selectedOption, handleOptionChange }: MultipleChoiceInputProps): JSX.Element {
     const answerOptions: string[] = [
         "Strongly Disagree",
         "Disagree",
@@ -16,32 +16,22 @@ export function MultipleChoiceInput({ currentQuestion }: MultipleChoiceInputProp
         "Strongly Agree"
     ];
 
-    useEffect(() => {
-        // Reset selected option when the question changes
-        setSelectedOption("");
-    }, [currentQuestion]); 
-    
-    const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSelectedOption(e.target.value);
-    };
     return (
         <div className="MultipleChoiceInput">
             <Form>
-            {answerOptions.map((answer, index) => (
-                <div className="radio-inline" key={index}>
-                    <Form.Check
-                        key={index}
-                        name="responseGroup" 
-                        type="radio"
-                        label={answer}
-                        value={answer}
-                        checked={selectedOption === answer}
-                        onChange={handleOptionChange} 
-                    />
-                </div>
+                {answerOptions.map((answer, index) => (
+                    <div className="radio-inline" key={index}>
+                        <Form.Check
+                            type="radio"
+                            name="responseGroup"
+                            label={answer}
+                            value={answer}
+                            checked={selectedOption === answer}
+                            onChange={handleOptionChange}
+                        />
+                    </div>
                 ))}
-               
             </Form>
         </div>
-    )
+    );
 }
