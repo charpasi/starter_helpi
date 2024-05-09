0bda282386e0956d41acc92c5b4b5414360f58c3
Roger Cronin <roger.m.cronin@gmail.com>
2024-05-09 12:05:19 -0400

    add by-word display

--- src/components/Genie.tsx
+++ src/components/Genie.tsx
@@ -1,5 +1,8 @@
 import "./Genie.css";
 import genie from "../assets/pikmin.png";
+import { useEffect, useState } from "react";
+
+const interval = 100;
 
 function Genie({
     text,
@@ -8,11 +11,32 @@ function Genie({
     text: string
     show: boolean
 }) {
+    const [displayText, setDisplayText] = useState<string>("");
+
+    useEffect(() => {
+        let currentIndex = 0;
+        const words = text.split(" ");
+        const intervalFunction = setInterval(() => {
+            if(currentIndex < words.length) {
+                setDisplayText(prevText => prevText + (words[currentIndex] ? words[currentIndex] : "") + " ");
+                currentIndex++;
+            } else {
+                clearInterval(intervalFunction);
+            }
+        }, interval);
+
+        return () => clearInterval(intervalFunction)
+    }, [text]);
+
+    useEffect(() => {
+        setDisplayText(text.split(" ")[0] + " ");
+    }, [text]);
+
     return (
         <div className={`Genie ${show ? "showing" : "not-showing"}`}>
             <img src={genie} alt="genie"/>
             {
-                text ? <p>{text}</p> : ""
+                text ? <p>{displayText}</p> : ""
             }
         </div>
     )
